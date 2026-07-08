036b9ad3013435bcb1ebee33eccb943d3122ea0c
Anuj Yadav <anujknp1206@gmail.com>
2026-07-08 23:37:19 +0530

    Update UserFactory

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,24 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use Notifiable, HasRoles;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
         'name',
         'email',
@@ -22,11 +31,21 @@ class User extends Authenticatable
         'dob',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
     protected function casts(): array
     {
         return [
@@ -35,6 +54,7 @@ class User extends Authenticatable
         ];
     }
 
+    // Relationships
     public function bookings()
     {
         return $this->hasMany(Booking::class);
@@ -45,3 +65,4 @@ class User extends Authenticatable
         return $this->hasOne(CustomerProfile::class);
     }
 }
+
